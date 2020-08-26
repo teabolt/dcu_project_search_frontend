84f87cb9d46dfcfaffd8ef6948dcd1c0c9a25ae0
teabolt <tomasbaltrunas0@gmail.com>
2020-08-26 22:50:21 +0100

    Use Material UI Accordion for Project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,88 +1,87 @@
-import React, { useState } from 'react';
+import className from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import FallbackPlaceholder from 'prosearch-components/FallbackPlaceholder';
-import LabelValuePairs from 'prosearch-components/LabelValuePairs';
 import StudentsList from 'prosearch-components/StudentsList';
 
 import './Project.scss';
 
 const Project = (props) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const onExpandControlClick = () => {
-    setExpanded(!expanded);
-  };
-
-  const ExpandControlIcon = expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />;
-
-  const items = [
-    {
-      label: 'Description',
-      pairClassName: 'project-description before-expanded',
-      value: <FallbackPlaceholder node={props.project.description} />,
-    },
-    ...(expanded
-      ? [
-          {
-            label: 'Programme',
-            pairClassName: 'project-programme',
-            value: <FallbackPlaceholder node={props.project.programme} />,
-          },
-          {
-            label: 'Supervisor',
-            pairClassName: 'project-supervisor',
-            value: <FallbackPlaceholder node={props.project.supervisor} />,
-          },
-          {
-            label: 'Students',
-            pairClassName: 'project-students',
-            value: (
+  return (
+    <div>
+      <Accordion className='project-container' key={props.key}>
+        <AccordionSummary
+          expandIcon={
+            <ExpandMoreIcon
+              className='project-expand-control'
+              color='inherit'
+            />
+          }
+        >
+          <div
+            className={className(
+              'label-value-pair',
+              'project-description before-expanded'
+            )}
+          >
+            <div className='project-header'>
+              <div className='project-title'>{props.project.title}</div>
+              <div className='project-year'>{props.project.year}</div>
+            </div>
+            <div className='label-value-pair-label'>Description</div>
+            <div className='label-value-pair-value'>
+              <FallbackPlaceholder node={props.project.description} />
+            </div>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className={className('label-value-pair', 'project-programme')}>
+            <div className='label-value-pair-label'>Programme</div>
+            <div className='label-value-pair-value'>
+              <FallbackPlaceholder node={props.project.programme} />
+            </div>
+          </div>
+          <div className={className('label-value-pair', 'project-supervisor')}>
+            <div className='label-value-pair-label'>Supervisor</div>
+            <div className='label-value-pair-value'>
+              <FallbackPlaceholder node={props.project.supervisor} />
+            </div>
+          </div>
+          <div className={className('label-value-pair', 'project-students')}>
+            <div className='label-value-pair-label'>Students</div>
+            <div className='label-value-pair-value'>
               <FallbackPlaceholder
                 node={<StudentsList students={props.project.students} />}
               />
-            ),
-          },
-          {
-            label: 'Area',
-            pairClassName: 'project-area',
-            value: <FallbackPlaceholder node={props.project.area} />,
-          },
-          {
-            label: 'Technology',
-            pairClassName: 'project-technology',
-            value: <FallbackPlaceholder node={props.project.technology} />,
-          },
-          ...(props.project.video
-            ? [
-                {
-                  label: 'Video',
-                  pairClassName: 'project-video',
-                  value: <FallbackPlaceholder node={props.project.video} />,
-                },
-              ]
-            : []),
-        ]
-      : []),
-  ];
-
-  return (
-    <div className='project-container' key={props.key}>
-      <div className='project-header'>
-        <div className='project-title'>{props.project.title}</div>
-        <div className='project-year'>{props.project.year}</div>
-      </div>
-      <LabelValuePairs className='project-properties' pairs={items} />
-      <IconButton
-        className='project-expand-control'
-        color='inherit'
-        onClick={onExpandControlClick}
-      >
-        {ExpandControlIcon}
-      </IconButton>
+            </div>
+          </div>
+          <div className={className('label-value-pair', 'project-area')}>
+            <div className='label-value-pair-label'>Area</div>
+            <div className='label-value-pair-value'>
+              <FallbackPlaceholder node={props.project.area} />
+            </div>
+          </div>
+          <div className={className('label-value-pair', 'project-technology')}>
+            <div className='label-value-pair-label'>Technology</div>
+            <div className='label-value-pair-value'>
+              <FallbackPlaceholder node={props.project.technology} />
+            </div>
+          </div>
+          {props.project.video && (
+            <div className={className('label-value-pair', 'project-video')}>
+              <div className='label-value-pair-label'>Video</div>
+              <div className='label-value-pair-value'>
+                <FallbackPlaceholder node={props.project.video} />
+              </div>
+            </div>
+          )}
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
