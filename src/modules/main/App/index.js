7d45c4afbcb21c4aa4fc444a31148ecbed9c4631
teabolt <tomasbaltrunas0@gmail.com>
2020-08-30 13:28:06 +0100

    Migrate to SCSS and add background overlay with gradient

--- src/modules/main/App/index.js
+++ src/modules/main/App/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AboutApp from 'modules/about';
 import SearchApp from 'modules/search';
 
+import BackgroundOverlay from 'modules/main/components/BackgroundOverlay';
 import Navbar from 'modules/main/components/Navbar';
 
 import './App.scss';
@@ -11,6 +12,7 @@ import './App.scss';
 function App() {
   return (
     <div className='App'>
+      <BackgroundOverlay />
       <BrowserRouter>
         <Navbar />
         <Switch>
---
+++ src/modules/main/components/BackgroundOverlay/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './BackgroundOverlay.scss';
+
+const BackgroundOverlay = () => {
+  return <div className='background-overlay'></div>;
+};
+
+export default BackgroundOverlay;
