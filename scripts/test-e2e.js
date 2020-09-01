a0490981b1d7ad84ab4cf0b76109512e82095de8
teabolt <tomasbaltrunas0@gmail.com>
2020-09-02 00:16:31 +0100

    Fix Sass loader and Cypress script exit codes

--- scripts/test-e2e.js
+++ scripts/test-e2e.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-process-exit */
+/* eslint-disable no-console */
 const cypress = require('cypress');
 
 const args = process.argv.slice(2);
@@ -5,5 +7,15 @@ const args = process.argv.slice(2);
 if (args.length > 0 && args[0] === '--dev') {
   cypress.open();
 } else {
-  cypress.run();
+  cypress
+    .run()
+    .then((results) => {
+      if (results.totalFailed) {
+        process.exit(1);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 }
