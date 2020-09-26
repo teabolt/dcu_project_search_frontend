2b543e2986c568f92b4b4ba748836972cca8b065
teabolt <tomasbaltrunas0@gmail.com>
2020-09-26 21:06:31 +0100

    Add logging middleware and update server

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 /* eslint-disable no-console */
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
-
 const paths = require('./config/paths');
 
 require('./config/env');
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
 const app = express();
 
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || '0.0.0.0';
+// Logging
+app.use(morgan('combined'));
 
+// Serve static files
 app.use(express.static(path.join(__dirname, paths.build)));
 
 app.get('/*', function (_req, res) {
   res.sendFile(path.join(__dirname, paths.build, 'index.html'));
 });
 
-app.listen(port, host, () => {
-  console.log(`App listening at http://${host}:${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`App listening at http://${HOST}:${PORT}`);
 });
