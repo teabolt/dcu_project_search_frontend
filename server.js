0d94dc8244c0a22c933817a789e367bbe7c6318d
teabolt <tomasbaltrunas0@gmail.com>
2020-09-27 21:42:32 +0100

    Add HTTPS support to server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-sync */
 /* eslint-disable no-console */
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
+
 const paths = require('./config/paths');
 
 require('./config/env');
@@ -21,6 +27,25 @@ app.get('/*', function (_req, res) {
   res.sendFile(path.join(__dirname, paths.build, 'index.html'));
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`App listening at http://${HOST}:${PORT}`);
-});
+const IS_HTTPS = process.env.NODE_ENV === 'production';
+
+let server;
+if (IS_HTTPS) {
+  const credentials = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT),
+  };
+  server = https.createServer(credentials, app);
+} else {
+  server = http.createServer(app);
+}
+
+if (server) {
+  server.listen({ port: PORT, host: HOST }, () => {
+    console.log(
+      `App listening at http${IS_HTTPS ? 's' : ''}://${HOST}:${PORT}`
+    );
+  });
+} else {
+  console.error(`Server is not created: ${server}`);
+}
